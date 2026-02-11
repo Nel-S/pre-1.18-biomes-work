315fd081224572e7a62809f9b3826b063086732c
NelS <75831544+Nel-S@users.noreply.github.com>
2026-02-10 16:01:05 -0800

    0.2.0 - Publish Notes - Inline comments + further testing - Switch Statistics layer salts to signed

---
+++ Notes.md
@@ -0,0 +1,44 @@
+# Notes
+
+Biomes are mostly selected using a 64-bit quadratic update function: `New = Next(State, Salt) = A*State^2 + B*State + Salt`.
+- `New` will always have the same parity as `Salt`.
+- Given `State` and `Salt`, one possibility for `New` exists; given `State` and `New`, one possibility for `Salt` exists.
+- Given `New` and `Salt`, two possibilities for State exist, one odd and one even.
+- The possibilities can be converted to one another via `possibility2 = -7379792620528906219LL - possibility1`.
+  - Across multiple advancements, whichever State possibility does not have parity matching Salt is impossible and is discarded; only during the very first advancement are both possibilites preserved. (This is where shadow seeds come from and why there are exactly two.)
+Ocean Temperature 256 additionally uses a single Perlin noisemap initialized with the structure seed.
+
+Each Layer has a layer salt that is version + generation mode-dependent.
+The worldseed is then used alongside the layer salt to generate the layer's start salt.
+The layer's start seed is `Next(start salt, 0)`. This will always be even, but only a multiple of `2^(n + 1)` if the start salt is a multiple of `2^n`.
+- Layers with identical layer salts should have identical start salts, start seeds, and subsequent PRNG streams.
+
+## Duplicate 1.16.1 Layer Salts (Unsigned):
+- 229918546094678885   (2001): Zoom 1024, Zoom 128 Ocean
+- 558114146894579477   (2005): Zoom 8 Ocean
+- 837738509879401688   (2002): Zoom 512, Zoom 64 Ocean
+- 1827289100522298840  (1002): Zoom 8, Zoom 8 River
+- 3006835321906069877  (2003): Zoom 256, Zoom 32 Ocean
+- 3038466749335869312  ( 200): Biome 256
+- 3107951898966440229  (   1): Continent 4096, Land 2048, River 4
+- 5360640171528462240  (   4): Land 256, Deep Ocean 256
+- 5692911206796425088  (1000): Zoom 128, Biome Edge 64, Zoom 128 Hills, Hills 64, Zoom 32, Shore 16, Smooth 4, Zoom 128 River, Zoom 32 River, Smooth 4 River
+- 5723240131506253216  ( 100): Noise 256, River Mix 4, Ocean Mix 4
+- 5852781679691581125  (1001): Bamboo 256, Zoom 64, Zoom 64 Hills, Sunflower 64, Zoom 16, Zoom 64 River, Zoom 16 River
+- 7231908362866731896  (  70): Land 1024 C
+- 7590731853067264053  (   3): Land 1024 D, Special 1024, Land 32
+- 9672642253349399552  (2000): Zoom 2048
+- 10938390704639815544 (2006): Zoom 4 Ocean
+- 10967462438749070293 (   5): Mushroom 256
+- 12314713599595444213 (1005): Zoom Large Biomes B
+- 13432066074785117656 (   2): Land 1024 A, Island 1024, Snow 1024, Cool 1024, Ocean Temperature 256
+- 14406777830260091477 (1003): Zoom 4, Zoom 4 River
+- 16630052651815956128 (1004): Zoom Large Biomes A
+- 16973349028156721880 (  50): Land 1024 B
+- 17944835642018066080 (2004): Zoom 16 Ocean
+- None (uses worldseed SHA)  : Voronoi 1
+
+## Next Steps:
+- Recreate Cubiomes' biome emulation without function chaining, to properly understand what each layer does + how they build off of their predecessors
+- What PRNG calls do layers with the same layer salt make? Do e.g. the salt 1000 ones give enough information to identify/significantly narrow down the possible original start salt?
+- Determine why rivers only depend on lowest 26 bits
--- test.c
+++ test.c
@@ -3,6 +3,11 @@
 #include "cubiomes/generator.h"
 #include "cubiomes/util.h"
 
+// Coefficients used in mcStepSeed.
+const uint64_t A = UINT64_C(6364136223846793005);
+const uint64_t B = UINT64_C(1442695040888963407);
+
+// Maps a layer enum to its corresponding name string.
 const char *layer2str(ptrdiff_t layer) {
 	switch (layer) {
 		case L_CONTINENT_4096: return "Continent 4096";
@@ -70,19 +75,26 @@ const char *layer2str(ptrdiff_t layer) {
 	}
 }
 
-bool printLayerStatistics() {
-	FILE *file = fopen(".\\Layer Statistics.txt", "w");
+// Prints version- and generation-mode-dependent layer statistics to a file.
+bool printLayerStatistics(const char *const filepath) {
+	if (!filepath) return false;
+	FILE *file = fopen(filepath, "w");
 	if (!file) return false;
 
 	LayerStack layerStack;
+	// For each version:
 	for (int version = MC_B1_8; version <= MC_1_17; ++version) {
+		// And both normal and large biomes (if 1.3+):
 		for (int largeBiomes = 0; largeBiomes <= (version >= MC_1_3); ++largeBiomes) {
 			fprintf(file, "%s%s\n---------------------------\n", mc2str(version), largeBiomes ? " Large Biomes" : "");
+			// Initialize Cubiomes' layer stack
 			setupLayerStack(&layerStack, version, largeBiomes);
 			for (size_t i = 0; i < sizeof(layerStack.layers)/sizeof(*layerStack.layers); ++i) {
+				// Cubiomes initially zero-fills all layers, so any uninitalized (and thus unused) layers will have all attributes still set to 0
 				Layer *layer = &layerStack.layers[i];
 				if (!layer->mc) continue;
-				fprintf(file, "%s:\n- Zoom: %" PRId8 "\n- Edge: %" PRId8 "\n- Scale: %d\n- Layer Salt: %" PRIu64 "\n- First Parent: %s\n- Second Parent: %s\n\n", layer2str(i), layer->zoom, layer->edge, layer->scale, layer->layerSalt, layer2str(layer->p - layerStack.layers), layer2str(layer->p2 - layerStack.layers));
+				// Print all relevant attributes for the current layer
+				fprintf(file, "%s:\n- Zoom: %" PRId8 "\n- Edge: %" PRId8 "\n- Scale: %d\n- Layer Salt: %" PRId64 "\n- First Parent: %s\n- Second Parent: %s\n\n", layer2str(i), layer->zoom, layer->edge, layer->scale, layer->layerSalt, layer2str(layer->p - layerStack.layers), layer2str(layer->p2 - layerStack.layers));
 			}
 			fprintf(file, "---------------------------\n");
 		}
@@ -99,8 +111,8 @@ bool testMcStepSeed(uint64_t internalState, uint64_t salt) {
 	return true;
 }
 
-// Quadratic congruence reversal logic from S. M. Dehnavi et al: https://doi.org/10.7546/nntdm.2019.25.1.75-83
-
+// Solves Ax^2 + Bx + C = 0 (mod 2^n) when A is even and B is odd.
+// Algorithm from S. M. Dehnavi et al (https://doi.org/10.7546/nntdm.2019.25.1.75-83).
 uint64_t solveWithEvenA(uint64_t evenA, uint64_t oddB, uint64_t c, size_t n) {
 	uint64_t output = 0;
 	for (size_t i = 0; i < n; ++i) {
@@ -114,12 +126,25 @@ uint64_t solveWithEvenA(uint64_t evenA, uint64_t oddB, uint64_t c, size_t n) {
 	return output;
 }
 
+// Returns the even solution for Ax^2 + Bx + C = 0 (mod 2^n) when A and B are odd, and C is even. (If C is also odd, no solution exists.)
+// Algorithm from S. M. Dehnavi et al (https://doi.org/10.7546/nntdm.2019.25.1.75-83).
+// TODO: Are any optimizations possible since A and B are fixed?
 uint64_t getEvenSolution(uint64_t oddA, uint64_t oddB, uint64_t evenC) {
 	return 2*solveWithEvenA(2*oddA, oddB, evenC/2, 63);
 }
 
+// Returns the odd solution for Ax^2 + Bx + C = 0 (mod 2^n) when A and B are odd, and C is even. (If C is also odd, no solution exists.)
+// Algorithm from S. M. Dehnavi et al (https://doi.org/10.7546/nntdm.2019.25.1.75-83).
+// TODO: Are any optimizations possible since A and B are fixed?
 uint64_t getOddSolution(uint64_t oddA, uint64_t oddB, uint64_t evenC) {
 	return 2*solveWithEvenA(2*oddA, 2*oddA + oddB, (oddA + oddB + evenC)/2, 63) + 1;
+}
+
+// Returns whichever of the two possible original states for mcStepSeed has the same parity as the salt/output. In the middle of sequential advancements, that is the only possible internal state.
+// For the very *first* mcStepSeed in the sequence, a second solution also exists that can be obtained via `possibility2 = -7379792620528906219LL - possibility1`.
+// WARNING: Output and Salt *must* have the same parity for this to work; otherwise no possible original states can exist.
+uint64_t reverseMcStepSeed(uint64_t output, uint64_t salt) {
+	return (salt & 1 ? getOddSolution : getEvenSolution)(A, B, salt - output);
 }
 
 bool testReverseMcStepSeed(uint64_t output, uint64_t salt) {
@@ -127,16 +152,40 @@ bool testReverseMcStepSeed(uint64_t output, uint64_t salt) {
 		printf("testReverseMcStepSeed(%" PRIu64 ", %" PRIu64 "): No solution exists.\n", output, salt);
 		return false;
 	}
-	uint64_t oddSolution = getOddSolution(6364136223846793005, 1442695040888963407, salt - output);
-	uint64_t evenSolution = getEvenSolution(6364136223846793005, 1442695040888963407, salt - output);
-	printf("Odd solution: %" PRIu64 " (Verification: %" PRIu64 " = %" PRIu64 ")\n", oddSolution, mcStepSeed(oddSolution, salt), output);
-	printf("Even solution: %" PRIu64 " (Verification: %" PRIu64 " = %" PRIu64 ")\n", evenSolution, mcStepSeed(evenSolution, salt), output);
+	uint64_t oddSolution = getOddSolution(A, B, salt - output);
+	uint64_t evenSolution = getEvenSolution(A, B, salt - output);
+	printf("Odd solution:  %20" PRIu64 " (Verification: %20" PRIu64 " = %20" PRIu64 ")\n", oddSolution, mcStepSeed(oddSolution, salt), output);
+	printf("Even solution: %20" PRIu64 " (Verification: %20" PRIu64 " = %20" PRIu64 ")\n", evenSolution, mcStepSeed(evenSolution, salt), output);
 	return true;
 }
 
 int main() {
-	// printLayerStatistics();
-	testMcStepSeed(8675309, 3107951898966440229);
-	testReverseMcStepSeed(mcStepSeed(8675309, 3107951898966440229), 3107951898966440229);
+	const uint64_t LAYER_SALT = 3107951898966440229;
+	// printLayerStatistics(".\\Layer Statistics.txt");
+	uint64_t state = 8675309;
+	for (size_t i = 0; i < 3; ++i) state = mcStepSeed(state, LAYER_SALT);
+
+	uint64_t states[(UINT64_C(1) << (3 + 1)) - 1];
+	states[0] = state;
+	for (size_t i = 0; i < (UINT64_C(1) << 3) - 1; ++i) {
+		if ((LAYER_SALT & 1) == (states[i] & 1)) {
+			states[2*i + 1] = getOddSolution(A, B, LAYER_SALT - states[i]);
+			printf("% 20" PRId64 " = % 20" PRId64 " <- (% 20" PRId64 ", % 20" PRId64 ")\n", states[i], mcStepSeed(states[2*i + 1], LAYER_SALT), states[2*i + 1], LAYER_SALT);
+			states[2*i + 2] = getEvenSolution(A, B, LAYER_SALT - states[i]);
+			printf("% 20" PRId64 " = % 20" PRId64 " <- (% 20" PRId64 ", % 20" PRId64 ")\n", states[i], mcStepSeed(states[2*i + 2], LAYER_SALT), states[2*i + 2], LAYER_SALT);
+		} else {
+			states[2*i + 1] = !(LAYER_SALT & 1);
+			states[2*i + 2] = !(LAYER_SALT & 1);
+		}
+	}
+	for (size_t i = (UINT64_C(1) << 3) - 1; i < sizeof(states)/sizeof(*states); ++i) {
+		// printf("% 20" PRId64 " -> % 20" PRId64 " -> % 20" PRId64 " -> % 20" PRId64 "\n", states[i], states[(i - 1)/2], states[((i - 1)/2 - 1)/2], states[(((i - 1)/2 - 1)/2 - 1)/2]);
+		printf("% 20" PRId64 " -> % 20" PRId64 ", % 20" PRId64 "\n", states[i], getStartSalt(states[i], LAYER_SALT), getStartSeed(states[i], LAYER_SALT));
+	}
+
+	
+
+	// testMcStepSeed(8675309, LAYER_SALT);
+	// testReverseMcStepSeed(mcStepSeed(8675309, LAYER_SALT), LAYER_SALT);
 	return 0;
 }
